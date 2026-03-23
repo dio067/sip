f045a8a0c42f2b81928e216a78978f462c6142fc
dio067 <yousefnas06@gmail.com>
2026-03-23 12:20:04 +0400

    feat: build TapButton component

---
+++ client/src/components/TapButton.jsx
@@ -0,0 +1,17 @@
+export default function TapButton({ onClick, disabled }) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className='
+        w-24 h-24 rounded-full
+        bg-sky-400 hover:bg-sky-500 active:scale-95
+        text-white text-4xl
+        shadow-lg shadow-sky-200
+        transition-all duration-150
+        disabled:opacity-40 disabled:cursor-not-allowed
+        select-none
+      '
+    ></button>
+  );
+}
